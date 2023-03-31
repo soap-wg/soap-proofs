279f1d76a900bd72e6c7ca664eb32a8fc28cddce
Felix Linker <linkerfelix@gmail.com>
2023-03-31 10:11:32 +0200

    Improve password related auxiliary lemmas

--- prove-all.sh
+++ prove-all.sh
@@ -1,4 +1,4 @@
 ./batch-run.sh SocialAuthentication --prove=SocialAuthentication
 ./batch-run.sh CodeVerifierSecrecy --prove=CodeVerifierSecrecy
-./batch-run.sh HelperLemmata --prove=BrowserSessionSources --prove=BrowserSessionBinding --prove=BrowserSessionUnique --prove=UsernamesUnique --prove=UsernamesServerConfirmed --prove=PasswordsConfidential --prove=SignalKeysUnique --prove=CodeIsSingleUse
+./batch-run.sh HelperLemmata --prove=BrowserSessionSources --prove=BrowserSessionBinding --prove=BrowserSessionUnique --prove=UsernamesUnique --prove=UsernamesServerConfirmed --prove=PasswordsConfidential --prove=SignalKeysUnique --prove=IsPW --prove=CodeIsSingleUse
 ./batch-run.sh SourcesLemma --prove=TokenFormatAndOTPLearning
